0dc3c02e7db94e093dc15030ed7a4f9e39be1bda
SpaceX50 <140992454+SpaceX50@users.noreply.github.com>
2023-08-02 14:57:59 +0700

    Update __init__.py

--- PyroUbot/core/plugins/__init__.py
+++ PyroUbot/core/plugins/__init__.py
@@ -18,6 +18,7 @@ from PyroUbot.core.plugins.ivt import *
 from PyroUbot.core.plugins.kng import *
 from PyroUbot.core.plugins.lgo import *
 from PyroUbot.core.plugins.lmt import *
+from PyroUbot.core.plugins.msc import *
 from PyroUbot.core.plugins.mmf import *
 from PyroUbot.core.plugins.mms import *
 from PyroUbot.core.plugins.nts import *
@@ -46,4 +47,3 @@ from PyroUbot.core.plugins.whs import *
 from PyroUbot.core.plugins.wrt import *
 from PyroUbot.core.plugins.ytp import *
 from PyroUbot.core.plugins.zbs import *
-from PyroUbot.core.plugins.msc import *
