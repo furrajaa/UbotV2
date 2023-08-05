b28b6e51a7d76da24f6ee09bc8290156f4f4cf86
SpaceX50 <140992454+SpaceX50@users.noreply.github.com>
2023-08-05 08:01:00 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -1,13 +1,19 @@
 import os
 
 API_ID = int(os.getenv("API_ID", "22039315"))
+
 API_HASH = os.getenv("API_HASH", "b9d878c0e74f6b2a2e2df3a1d9be968b")
+
 BOT_TOKEN = os.getenv("BOT_TOKEN", "6207982225:AAFqWP9qDE3fmvxH21W6Ppgui8yZwTdubAk")
+
 OWNER_ID = int(os.getenv("OWNER_ID", "1964437366"))
+
 LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1001969856888"))
-BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1001969856888").split()))
+
 MAX_BOT = int(os.getenv("MAX_BOT", "50"))
+
 RMBG_API = os.getenv("RMBG_API", "b5ZnjZ2nUUpbdEHfcrWdjWbC")
+
 OPENAI_KEY = os.getenv(
     "OPENAI_KEY", "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"
 )
