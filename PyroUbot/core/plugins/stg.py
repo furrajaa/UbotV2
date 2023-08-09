ef0ebaeaca571b6979688ab468d5795600e5d75c
SpaceX50 <140992454+SpaceX50@users.noreply.github.com>
2023-08-09 14:13:29 +0700

    Update stg.py

--- PyroUbot/core/plugins/stg.py
+++ PyroUbot/core/plugins/stg.py
@@ -1,7 +1,7 @@
-import re
+import emoji
+emoji._emoji_lis = emoji.EMOJI_UNICODE.values()
 
 from PyroUbot import *
-
 
 async def setprefix(client, message):
     Tm = await message.reply("ᴍᴇᴍᴘʀᴏsᴇs...", quote=True)
@@ -18,9 +18,7 @@ async def setprefix(client, message):
             ubot.set_prefix(message.from_user.id, ub_prefix)
             await set_pref(message.from_user.id, ub_prefix)
             parsed_prefix = " ".join(f"<code>{prefix}</code>" for prefix in ub_prefix)
-            parsed_prefix = re.sub(
-                r"([\U0001F1E0-\U0001F1FF])", r"<b>\1</b>", parsed_prefix
-            )
             return await Tm.edit(f"<b>✅ ᴘʀᴇғɪx ᴛᴇʟᴀʜ ᴅɪᴜʙᴀʜ ᴋᴇ: {parsed_prefix}</b>")
         except Exception as error:
             return await Tm.edit(str(error))
+            
