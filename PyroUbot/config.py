bc415328443759669a0abccfc4ca3307393a75c2
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-08-16 14:46:47 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -15,7 +15,7 @@ MAX_BOT = int(os.getenv("MAX_BOT", "50"))
 RMBG_API = os.getenv("RMBG_API", "b5ZnjZ2nUUpbdEHfcrWdjWbC")
 
 OPENAI_KEY = os.getenv(
-    "OPENAI_KEY", "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"
+    "OPENAI_KEY", "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"
 )
 MONGO_URL = os.getenv(
     "MONGO_URL",
