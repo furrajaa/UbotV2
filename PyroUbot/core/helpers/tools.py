2e0da88cb3c19fff59f21059cca545871792bbaa
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-08-17 00:54:29 +0700

    Update tools.py

--- PyroUbot/core/helpers/tools.py
+++ PyroUbot/core/helpers/tools.py
@@ -14,14 +14,9 @@ from pyrogram.errors import *
 
 
 def get_message(message):
-    if message.reply_to_message:
-        msg = message.reply_to_message
-    else:
-        if len(message.command) < 2:
-            msg = ""
-        else:
-            msg = " ".join(message.command[1:])
+    msg = message.reply_to_message if message.reply_to_message else "" if len(message.command) < 2 else " ".join(message.command[1:])
     return msg
+
 
 
 async def get_broadcast_id(client, query):
