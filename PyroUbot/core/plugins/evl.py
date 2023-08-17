ab894b226bcbf1741cfcdc9b73daf29507ae0628
XtomiX <XtomiX@users.noreply.github.com>
2023-08-17 05:55:21 +0000

    Auto Fix

--- PyroUbot/core/plugins/evl.py
+++ PyroUbot/core/plugins/evl.py
@@ -11,23 +11,15 @@ import psutil
 from PyroUbot import *
 
 
-import os
-import subprocess
-import sys
-import platform
-import psutil
-from datetime import datetime
-from io import BytesIO
-
 async def shell_cmd(client, message):
     command = message.command
-    
+
     if len(command) < 2:
         return await message.reply("noob")
-    
+
     try:
         action = command[1]
-        
+
         if action == "shutdown":
             await handle_shutdown(message)
         elif action == "restart":
@@ -40,29 +32,33 @@ async def shell_cmd(client, message):
             await handle_host(message)
         else:
             await process_command(message, command[1])
-    
+
     except Exception as error:
         await message.reply(error)
+
 
 async def handle_shutdown(message):
     await message.reply("✅ System berhasil dimatikan", quote=True)
     os.system(f"kill -9 {os.getpid()}")
 
+
 async def handle_restart(message):
     await message.reply("✅ System berhasil direstart", quote=True)
     os.execl(sys.executable, sys.executable, "-m", "PyroUbot")
 
+
 async def handle_update(message):
     out = subprocess.check_output(["git", "pull"]).decode("UTF-8")
-    
+
     if "Already up to date." in str(out):
         return await message.reply(out, quote=True)
     elif int(len(str(out))) > 4096:
         await send_large_output(message, out)
     else:
         await message.reply(f"```{out}```", quote=True)
-        
+
     os.execl(sys.executable, sys.executable, "-m", "PyroUbot")
+
 
 async def handle_clean(message):
     count = 0
@@ -75,16 +71,18 @@ async def handle_clean(message):
     await bash("rm -rf downloads")
     await message.reply(f"✅ {count} sampah berhasil di bersihkan")
 
+
 async def handle_host(message):
     system_info = get_system_info()
     formatted_info = format_system_info(system_info)
-    
+
     await message.reply(formatted_info, quote=True)
+
 
 async def process_command(message, command):
     msg = await message.reply("<b>Memproses</b>")
     result = (await bash(command))[0]
-    
+
     if int(len(str(result))) > 4096:
         await send_large_output(message, result)
         await msg.delete()
@@ -92,16 +90,18 @@ async def process_command(message, command):
         await message.reply(result)
         await msg.delete()
 
+
 async def send_large_output(message, output):
     with BytesIO(str.encode(str(output))) as out_file:
         out_file.name = "result.txt"
         await message.reply_document(document=out_file)
-        
+
+
 def get_system_info():
     uname = platform.uname()
     cpufreq = psutil.cpu_freq()
     svmem = psutil.virtual_memory()
-    
+
     return {
         "system": uname.system,
         "release": uname.release,
@@ -113,15 +113,18 @@ def get_system_info():
         "cpu_max_frequency": cpufreq.max,
         "cpu_min_frequency": cpufreq.min,
         "cpu_current_frequency": cpufreq.current,
-        "cpu_percent_per_core": [percentage for percentage in psutil.cpu_percent(percpu=True)],
+        "cpu_percent_per_core": [
+            percentage for percentage in psutil.cpu_percent(percpu=True)
+        ],
         "cpu_total_usage": psutil.cpu_percent(),
         "network_upload": get_size(psutil.net_io_counters().bytes_sent),
         "network_download": get_size(psutil.net_io_counters().bytes_recv),
         "memory_total": get_size(svmem.total),
         "memory_available": get_size(svmem.available),
         "memory_used": get_size(svmem.used),
-        "memory_percentage": svmem.percent
+        "memory_percentage": svmem.percent,
     }
+
 
 def format_system_info(system_info):
     formatted_info = "Informasi Sistem\n"
@@ -130,22 +133,26 @@ def format_system_info(system_info):
     formatted_info += f"Versi    : {system_info['version']}\n"
     formatted_info += f"Mesin    : {system_info['machine']}\n"
 
-    boot_time = datetime.fromtimestamp(system_info['boot_time'])
+    boot_time = datetime.fromtimestamp(system_info["boot_time"])
     formatted_info += f"Waktu Hidup: {boot_time.day}/{boot_time.month}/{boot_time.year}  {boot_time.hour}:{boot_time.minute}:{boot_time.second}\n"
 
     formatted_info += "\nInformasi CPU\n"
-    formatted_info += "Physical cores   : " + str(system_info['cpu_physical_cores']) + "\n"
-    formatted_info += "Total cores      : " + str(system_info['cpu_total_cores']) + "\n"
+    formatted_info += (
+        "Physical cores   : " + str(system_info["cpu_physical_cores"]) + "\n"
+    )
+    formatted_info += "Total cores      : " + str(system_info["cpu_total_cores"]) + "\n"
     formatted_info += f"Max Frequency    : {system_info['cpu_max_frequency']:.2f}Mhz\n"
     formatted_info += f"Min Frequency    : {system_info['cpu_min_frequency']:.2f}Mhz\n"
-    formatted_info += f"Current Frequency: {system_info['cpu_current_frequency']:.2f}Mhz\n\n"
+    formatted_info += (
+        f"Current Frequency: {system_info['cpu_current_frequency']:.2f}Mhz\n\n"
+    )
     formatted_info += "CPU Usage Per Core\n"
-    
-    for i, percentage in enumerate(system_info['cpu_percent_per_core']):
+
+    for i, percentage in enumerate(system_info["cpu_percent_per_core"]):
         formatted_info += f"Core {i}  : {percentage}%\n"
     formatted_info += "Total CPU Usage\n"
     formatted_info += f"Semua Core: {system_info['cpu_total_usage']}%\n"
-    
+
     formatted_info += "\nBandwith Digunakan\n"
     formatted_info += f"Unggah  : {system_info['network_upload']}\n"
     formatted_info += f"Download: {system_info['network_download']}\n"
@@ -155,9 +162,8 @@ def format_system_info(system_info):
     formatted_info += f"Available : {system_info['memory_available']}\n"
     formatted_info += f"Used      : {system_info['memory_used']}\n"
     formatted_info += f"Percentage: {system_info['memory_percentage']}%\n"
-    
-    return f"<b>{Fonts.smallcap(formatted_info.lower())}</b>"
 
+    return f"<b>{Fonts.smallcap(formatted_info.lower())}</b>"
 
 
 async def evalator_cmd(client, message):
