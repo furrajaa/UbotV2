881f2081737b56e200e133d7eefbc983068dbc6c
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-08-18 16:47:33 +0700

    Create waifu_api.py

---
+++ PyroUbot/core/helpers/waifu_api.py
@@ -0,0 +1,11 @@
+import requests, random
+
+def hentai_api()
+    url = "https://www.waifu.im/search/?included_tags=hentai"
+    response = requests.get(url)
+    content = response.text
+    start_index = content.find("var files = [") + len("var files = ")
+    end_index = content.find("]", start_index)
+    files_str = content[start_index:end_index]
+    files = [file.strip('" ') for file in files_str.split(",")]
+    return random.choice(files)
