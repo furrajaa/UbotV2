51f71f45e8f203f936bea4f605389eb05613d7fd
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-08-19 16:28:41 +0700

    Update anime.py

--- PyroUbot/modules/anime.py
+++ PyroUbot/modules/anime.py
@@ -1,6 +1,16 @@
 from PyroUbot import *
 
 
+__MODULE__ = "anime"
+__HELP__ = """
+<b>『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ᴀɴɪᴍᴇ 』</b>
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{0}wall</code>
+                     <code>{0}hentai</code>
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢɪʀɪᴍ ᴘʜᴏᴛᴏ ᴀɴɪᴍᴇ ʀᴀɴᴅᴏᴍ
+"""
+
+
 @PY.UBOT("wall|hentai")
 async def _(client, message):
     await anime_cmd(client, message)
