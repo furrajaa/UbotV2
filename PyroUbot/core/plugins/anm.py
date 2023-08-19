fc84b8f8f2f5aaa9f5bbf3ce74577c97c309d2e2
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-08-19 16:16:32 +0700

    Update anm.py

--- PyroUbot/core/plugins/anm.py
+++ PyroUbot/core/plugins/anm.py
@@ -3,7 +3,14 @@ from PyroUbot import *
 
 async def anime_cmd(client, message):
     msg = await message.reply("<b>ᴛᴜɴɢɢᴜ sᴇʙᴇɴᴛᴀʀ</b>", quote=True)
-    if message.command[0] == "hentai":
+    if message.command[0] == "wall":
+        photo = API.wall(client)
+        try:
+            await message.reply_photo(photo, quote=True)
+            return await msg.delete()
+        except Exception as error:
+            return await msg.edit(error)
+    elif message.command[0] == "hentai":
         photo = API.nsfw()
         try:
             await message.reply_photo(photo, quote=True)
