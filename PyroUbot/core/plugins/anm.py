f89d9fe0c477c877a78a788466b00edd61baeffe
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-08-19 16:32:41 +0700

    Update anm.py

--- PyroUbot/core/plugins/anm.py
+++ PyroUbot/core/plugins/anm.py
@@ -4,7 +4,7 @@ from PyroUbot import *
 async def anime_cmd(client, message):
     msg = await message.reply("<b>ᴛᴜɴɢɢᴜ sᴇʙᴇɴᴛᴀʀ</b>", quote=True)
     if message.command[0] == "wall":
-        photo = API.wall(client)
+        photo = await API.wall(client)
         try:
             await message.reply_photo(photo, quote=True)
             return await msg.delete()
