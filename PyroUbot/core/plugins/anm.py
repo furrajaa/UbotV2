2a6927e11ff26d2e87b32ee83b9fe555ca5f56c3
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-08-19 16:05:34 +0700

    Update and rename wfu.py to anm.py

---
+++ PyroUbot/core/plugins/anm.py
@@ -0,0 +1,12 @@
+from PyroUbot import *
+
+
+async def anime_cmd(client, message):
+    msg = await message.reply("<b>ᴛᴜɴɢɢᴜ sᴇʙᴇɴᴛᴀʀ</b>", quote=True)
+    if message.command[0] == "hentai";
+        photo = API.nsfw()
+        try:
+            await message.reply_photo(photo, quote=True)
+            return await msg.delete()
+        except Exception as error:
+            return await msg.edit(error)
--- PyroUbot/core/plugins/wfu.py
+++
@@ -1,6 +0,0 @@
-from PyroUbot import *
-
-
-async def waifu_cmd(client, message):
-    photo = Waifu.nsfw()
-    await message.reply_photo(photo, quote=True)
