a8788e3464a977b835c0a97130e556d55b858de5
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-08-20 09:31:55 +0700

    Update str.py

--- PyroUbot/core/plugins/str.py
+++ PyroUbot/core/plugins/str.py
@@ -44,8 +44,8 @@ async def ping_cmd(client, message):
     delta_ping = (end - start).microseconds / 1000
     if client.me.is_premium:
         _ping = f"""
-<b><emoji id=5335018876839141938>✅</emoji> ᴘᴏɴɢ:</b> <code>{str(delta_ping).replace('.', ',')} ms</code>
-<b><emoji id=5971885530957222021>🔹</emoji> ᴜᴘᴛɪᴍᴇ:</b> <code>{uptime}</code>
+<b><emoji id=4999015678238262018>✨</emoji> ᴘᴏɴɢ:</b> <code>{str(delta_ping).replace('.', ',')} ms</code>
+<b><emoji id=5017179932451668652>🕖</emoji> ᴜᴘᴛɪᴍᴇ:</b> <code>{uptime}</code>
 """
     else:
         _ping = f"""
