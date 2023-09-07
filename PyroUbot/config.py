937072fdd2c729d35f0031ea914674c45981e46b
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-09-07 13:32:18 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -1,25 +1,23 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "22039315"))
+from dotenv import load_dotenv
 
-API_HASH = os.getenv("API_HASH", "b9d878c0e74f6b2a2e2df3a1d9be968b")
+load_dotenv(".env")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "6207982225:AAFqWP9qDE3fmvxH21W6Ppgui8yZwTdubAk")
+API_ID = int(os.getenv("API_ID"))
 
-OWNER_ID = int(os.getenv("OWNER_ID", "1964437366"))
+API_HASH = os.getenv("API_HASH")
 
-LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1001969856888"))
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-MAX_BOT = int(os.getenv("MAX_BOT", "100"))
+OWNER_ID = int(os.getenv("OWNER_ID"))
 
-RMBG_API = os.getenv("RMBG_API", "b5ZnjZ2nUUpbdEHfcrWdjWbC")
+LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT"))
 
-OPENAI_KEY = os.getenv(
-    "OPENAI_KEY",
-    "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 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 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 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 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 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 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 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",
-).split()
+MAX_BOT = int(os.getenv("MAX_BOT"))
 
-MONGO_URL = os.getenv(
-    "MONGO_URL",
-    "mongodb+srv://PremUbot:sST5VL04n85JkcxS@cluster0.wixneuz.mongodb.net/?retryWrites=true&w=majority",
-)
+RMBG_API = os.getenv("RMBG_API")
+
+OPENAI_KEY = os.getenv("OPENAI_KEY")
+
+MONGO_URL = os.getenv("MONGO_URL")
