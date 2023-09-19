e6679f72f0b8743509cff64909ed7724dd58e0fb
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-09-20 04:25:59 +0700

    Update stg.py

--- PyroUbot/core/plugins/stg.py
+++ PyroUbot/core/plugins/stg.py
@@ -18,4 +18,26 @@ async def setprefix(client, message):
             parsed_prefix = " ".join(f"<code>{prefix}</code>" for prefix in ub_prefix)
             return await Tm.edit(f"<b>✅ ᴘʀᴇғɪx ᴛᴇʟᴀʜ ᴅɪᴜʙᴀʜ ᴋᴇ: {parsed_prefix}</b>")
         except Exception as error:
-            return await Tm.edit(str(error))
+            return await Tm.edit(str(error)
+
+
+async def change_emot(client, message):
+    Tm = await message.reply("ᴍᴇᴍᴘʀᴏsᴇs...", quote=True)
+    if len(message.command) < 3:
+        return await msg.edit("<b>ᴛᴏʟᴏɴɢ ᴍᴀsᴜᴋᴋᴀɴ ǫᴜᴇʀʏ ᴅᴀɴ ᴠᴀʟᴇᴜ ɴʏᴀ</b>")
+    query_mapping = {"pong": "EMOJI_PING_PONG", "uptime": "EMOJI_UPTIME"}
+    command, mapping, valeu = message.command[:3]
+    if mapping.lower() in query_mapping:
+        query_var = query_mapping[mapping.lower()]
+        await set_vars(client.me.id, query_var, valeu)
+            return await msg.edit(
+                f"<b>✅ <code>{query_var}</code> ʙᴇʀʜᴀsɪʟ ᴅɪ sᴇᴛᴛɪɴɢ ᴋᴇ: <code><b><emoji id={valeu}>😎</emoji></code></b>"
+            )
+        else:
+            return await msg.edit("<b>ǫᴜᴇʀʏ ʏᴀɴɢ ᴅɪ ᴍᴀsᴜᴋᴋᴀɴ sᴀʟᴀʜ</b>")
+    except Exception as error:
+        await msg.edit(str(error))
+
+
+
+
