87f1f7fd64468b5058697c021e7e0b2f4f7fee38
SpaceX50 <140992454+SpaceX50@users.noreply.github.com>
2023-11-05 19:13:46 +0700

    Update variabel.py

--- PyroUbot/core/database/variabel.py
+++ PyroUbot/core/database/variabel.py
@@ -15,3 +15,27 @@ async def get_vars(bot_id, vars_name):
 
 async def remove_all_vars(bot_id):
     await varsdb.delete_one({"_id": bot_id})
+
+
+async def remove_vars(bot_id, vars_name, query="vars"):
+    remove_data = {"$unset": {f"{query}.{vars_name}": ""}}
+    await varsdb.update_one({"_id": bot_id}, remove_data)
+
+
+async def get_pm_id(user_id):
+    pm_id = await get_vars(user_id, "PM_PERMIT")
+    return [int(x) for x in str(pm_id).split()] if pm_id else []
+
+
+async def add_pm_id(me_id, user_id):
+    pm_id = await get_vars(me_id, "PM_PERMIT")
+    if pm_id:
+        user_id = f"{pm_id} {user_id}"
+    await set_vars(me_id, "PM_PERMIT", user_id)
+
+
+async def remove_pm_id(me_id, user_id):
+    pm_id = await get_vars(me_id, "PM_PERMIT")
+    if pm_id:
+        list_id = [int(x) for x in str(pm_id).split() if x != str(user_id)]
+        await set_vars(me_id, "PM_PERMIT", " ".join(map(str, list_id)))
