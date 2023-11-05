98a073dabd01e09d32eeb3d1125acc8070d2b5ce
SpaceX50 <140992454+SpaceX50@users.noreply.github.com>
2023-11-05 19:12:31 +0700

    Update client.py

--- PyroUbot/core/helpers/client.py
+++ PyroUbot/core/helpers/client.py
@@ -13,6 +13,22 @@ class FILTERS:
 
 
 class PY:
+    def AFK():
+        def wrapper(func):
+            afk_check = (
+                (filters.mentioned | filters.private)
+                & ~filters.bot
+                & ~filters.me
+                & filters.incoming
+            )
+
+            @ubot.on_message(afk_check, group=7)
+            async def wrapped_func(client, message):
+                await func(client, message)
+
+            return wrapped_func
+
+        return wrapper
     def BOT(command, filter=FILTERS.PRIVATE):
         def wrapper(func):
             @bot.on_message(filters.command(command) & filter)
