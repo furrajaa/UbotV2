42809710e114a721dcae8b1cbfa94af85250806a
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-02-21 19:37:31 +0700

    Update start.py

--- PyroUbot/modules/start.py
+++ PyroUbot/modules/start.py
@@ -4,3 +4,7 @@ from .. import *
 @PY.BOT("start")
 async def _(client, message):
     await start_cmd(client, message)
+
+@PY.UBOT("ping")
+async def _(client, message):
+    await ping_cmd(client, message)
